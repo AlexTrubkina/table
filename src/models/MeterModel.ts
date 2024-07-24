3b00ff12faa7cddc93ab6b1527689713bb96628a
AlexTrubkina <sashatru99@gmail.com>
2024-07-24 16:53:52 +0300

    feat: fix types of MetersModel

--- src/models/MeterModel.ts
+++ src/models/MeterModel.ts
@@ -1,21 +1,24 @@
-import {t} from 'mobx-state-tree'
+import { t } from "mobx-state-tree";
 
-const AreaModel = t.model('AreaModel', {
+const AreaModel = t.model("AreaModel", {
     id: t.string,
-})
+});
 
-export const MeterModel = t.model('MeterModel', {
-    id: t.string,
+export const MeterModel = t.model("MeterModel", {
+    id: t.maybeNull(t.string),
     _type: t.array(t.string),
     area: AreaModel,
-    is_automatic: t.boolean,
-    communication: t.string,
-    description: t.string,
-    serial_number: t.string,
-    installation_date: t.string,
-    brand_name: t.string,
-    model_name: t.string,
+    is_automatic: t.maybeNull(t.boolean),
+    communication: t.maybeNull(t.string),
+    description: t.maybeNull(t.string),
+    serial_number: t.maybeNull(t.string),
+    installation_date: t.maybeNull(t.string),
+    brand_name: t.maybeNull(t.string),
+    model_name: t.maybeNull(t.string),
     initial_values: t.array(t.number),
+});
+
+export const Pages = t.model("Pages", {
+    current: t.number,
+    total: t.number
 })
-
-
